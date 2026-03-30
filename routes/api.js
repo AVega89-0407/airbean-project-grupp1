4adae7806f608c5dcc51f743cae8230dbaa35cee
Angelica Jonsson Landström <angelicajonsson24@gmail.com>
2026-03-30 09:59:59 +0200

    Fix menu handling and update related routes

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { requireApiKey } from "../middleware/requireApiKey.js";
 import userRoutes from "./userRoutes.js";
+import menuRoutes from "./menuRoutes.js";
 
 const router = Router();
 
 router.use(requireApiKey);
 router.use('/users', userRoutes);
+router.use('/menu', menuRoutes);
 
 export default router;
