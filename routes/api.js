610595defddd489ac0cac965cdb98bf7c7c67c3c
AVega89-0407 <andreavega89.av@gmail.com>
2026-03-30 18:20:59 +0200

    sparar lokala ändringar

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,12 @@ import { Router } from 'express';
 import { requireApiKey } from "../middleware/requireApiKey.js";
 import userRoutes from "./userRoutes.js";
 
+import orderRoutes from "./ordersRoutes.js";
+
 const router = Router();
 
 router.use(requireApiKey);
 router.use('/users', userRoutes);
+router.use('/orders', orderRoutes);
 
 export default router;
