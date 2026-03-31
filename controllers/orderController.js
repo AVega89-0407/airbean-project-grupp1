dce78d040ddaf16e2ff9b2575cb1e82ae16a73cd
Angelica Jonsson Landström <angelicajonsson24@gmail.com>
2026-03-31 08:34:34 +0200

    beställnings kontroll

---
+++ controllers/orderController.js
@@ -0,0 +1,17 @@
+import crypto from 'crypto';
+
+export function createOrder(req, res) {
+    const {items, total, userId} = req.body;
+
+const orderId = crypto.randomUUID();
+const eta = Math.floor(Math.random() *10) + 5 + orderId.items.length * 2;
+
+res.status(201).json({
+    orderId,
+    eta,
+    items,
+    total,
+    userId: userId || null,
+    message: `Order mottagen! Din beställning kommer att vara klar om ca ${eta} minuter.`
+});
+}
